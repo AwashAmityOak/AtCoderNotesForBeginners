799c636cab6e6a985bacaf6d67d969e3ec52c98f
AwashAmityOak <117988946+AwashAmityOak@users.noreply.github.com>
2024-03-23 16:15:46 +0900

    Updated version number

--- src/AtCoderNotesForBeginners.user.js
+++ src/AtCoderNotesForBeginners.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AtCoderNotesForBeginners
 // @namespace    https://github.com/AwashAmityOak
-// @version      0.2.0
+// @version      0.2.1
 // @description  Show some knowledges for beginners on AtCoder.
 // @author       AwashAmityOak
 // @match        https://atcoder.jp/contests/*/tasks/*
